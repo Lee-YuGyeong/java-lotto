05d2f1c3c353a226e3bf7eaed3e26a8d0e23e8cf
YuGyeong <lyg64522@naver.com>
2025-04-08 21:17:56 +0900

    test: 로또 당첨 개수 테스트 코드 추가
    - 로또 당첨 개수 테스트 코드 추가

--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -43,12 +43,28 @@ class WinningNumberTest {
 
 
         LottoRow row1 = new LottoRow(List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(7))); // 2등
-        LottoRow row2 = new LottoRow(List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(8))); // 3등
-        LottoTicket lottoTicket = new LottoTicket(List.of(row1, row2));
+        LottoTicket lottoTicket = new LottoTicket(List.of(row1));
 
         Map<Prize, Integer> winningList = winningNumber.calculate(lottoTicket);
 
         assertThat(winningList.get(Prize.SECOND)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("당첨 로또 개수 계산_3등")
+    void 당첨_로또_개수_계산_3등() {
+
+        WinningNumber winningNumber = new WinningNumber(List.of(
+            new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4),
+            new LottoNumber(5), new LottoNumber(6))
+            , new LottoNumber(7));
+
+
+        LottoRow row2 = new LottoRow(List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(8))); // 3등
+        LottoTicket lottoTicket = new LottoTicket(List.of(row2));
+
+        Map<Prize, Integer> winningList = winningNumber.calculate(lottoTicket);
+
         assertThat(winningList.get(Prize.THIRD)).isEqualTo(1);
     }
 }
